9ba02b31fa993d942989820b22b5692141ccc437
Colin McKibben <mckibbenc@gmail.com>
2018-10-02 20:29:32 -0400

    Formatting changes

--- LexPerm.py
+++ LexPerm.py
@@ -1,6 +1,6 @@
 # Calculates the total possible permutations given the
-# number of characters
-
+# number of characters.  This is the iterative approach to
+# calculating factorials.
 
 def calcPerms(size):
     permutations = 1
@@ -8,9 +8,9 @@ def calcPerms(size):
         permutations = i * permutations
     return permutations
 
-# Recursively calculates the total possible permutations given
-# the number of characters
 
+# Recursively calculates the total possible permutations given
+# the number of characters.  This is the definition of a factorial.
 
 def calcPermsRecursive(n):
     if n > 1:
@@ -18,45 +18,49 @@ def calcPermsRecursive(n):
     else:
         return 1
 
+
 # Get the nth permutation of the input character array, where
 # permutation 1 is the input character array.
+# -- a set is the number of permutatations where index 0 is constant
+#    (ex. [0,1,2] and [0,2,1] are a set)
 
+def getNthPermutation(chars, nthPerm):
+    if (nthPerm == 0):
+        return []
 
-def getNthPermutation(chars, n):
     permutations = calcPerms(len(chars))
-    x = permutations // len(chars)
-    nthPermutation = []
+    setSize = permutations // len(chars)
+    result = []
 
     while len(chars) > 1:
-        if n % x != 0:
-            nthPermutation.append(chars.pop(n // x))
-            n = n % x
+        if nthPerm % setSize != 0:
+            result.append(chars.pop(nthPerm // setSize))
+            nthPerm = nthPerm % setSize
         else:
-            nthPermutation.append(chars.pop(n // x - 1))
-            n = n // (n // x)
-        permutations = x
-        x = permutations // len(chars)
+            result.append(chars.pop(nthPerm // setSize - 1))
+            nthPerm = nthPerm // (nthPerm // setSize)
+        permutations = setSize
+        setSize = permutations // len(chars)
 
-    nthPermutation.append(chars.pop(0))
-    return nthPermutation
+    result.append(chars.pop(0))
+    return result
 
 
 # Get the permutation number of the input character array assuming
 # permutation 1 is the input sorted.
 
-
 def getPermutationNumber(chars):
     sortedCharArray = chars[:]  # deep copy the array
     sortedCharArray.sort()
     permutations = calcPerms(len(sortedCharArray))
-    x = permutations // len(sortedCharArray)
+    setSize = permutations // len(sortedCharArray)
     nthPermutation = 0
 
     for i in range(0, len(chars) - 1):
-        nthPermutation += x * sortedCharArray.index(chars[i])
+        nthPermutation += setSize * sortedCharArray.index(chars[i])
         sortedCharArray.pop(sortedCharArray.index(chars[i]))
-        permutations = x
-        x = permutations // len(sortedCharArray)
+        permutations = setSize
+        setSize = permutations // len(sortedCharArray)
     nthPermutation += 1
 
     return nthPermutation
